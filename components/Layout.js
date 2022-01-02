0b9845b355996873bd86e8ed5de755f095ab538d
Idan Bernard <idanber1234@gmail.com>
2022-01-02 21:47:24 +0200

    more layout updates

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,7 @@ import TopBar from './topbar/TopBar';
 function Layout({ children, currentUser }) {
     const menuState = dataStore(state => state.isMenuOpen);
     return (
-        <div className='min-w-fit w-full h-[200vh] bg-gray-900 bg-fixed'>
+        <div className='min-w-fit w-full h-[200vh] bg-gray-900'>
             <TopBar currentUser={currentUser} />
             <SideBar currentUser={currentUser} />
 
